a1426035184753cda4c84606ffa4e929cef5f1a2
Danilo Bargen <mail@dbrgn.ch>
2022-01-09 00:47:34 +0100

    Firmware: Only import lib targets in test mode

--- firmware/src/lib.rs
+++ firmware/src/lib.rs
@@ -1,8 +1,13 @@
 // This module is only used as an entry point for unit testing.
 #![cfg_attr(not(test), no_std)]
 
+#[cfg(test)]
 mod ambient_light;
+#[cfg(test)]
 mod door_sensors;
+#[cfg(test)]
 mod errors;
+#[cfg(test)]
 mod serial;
+#[cfg(test)]
 mod states;
