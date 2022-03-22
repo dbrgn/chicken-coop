0e0d9e7e6255533639ef45d820d1c4d73854b1eb
Danilo Bargen <mail@dbrgn.ch>
2022-03-22 23:17:18 +0100

    rpi: Add nginx RTMP module

---
+++ rpi-image/package/nginx-rtmp-module/nginx-rtmp-module.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# nginx-rtmp-mdoule
+#
+################################################################################
+
+NGINX_RTMP_MODULE_VERSION = 1.2.2
+NGINX_RTMP_MODULE_SITE = $(call github,arut,nginx-rtmp-module,v$(NGINX_RTMP_MODULE_VERSION))
+NGINX_RTMP_MODULE_LICENSE = BSD-2-Clause
+NGINX_RTMP_MODULE_LICENSE_FILES = LICENSE
+NGINX_RTMP_MODULE_DEPENDENCIES = openssl
+
+NGINX_DEPENDENCIES += nginx-rtmp-module
+NGINX_CONF_OPTS += --add-module=$(NGINX_RTMP_MODULE_DIR)
+
+$(eval $(generic-package))
